51b9d3b7180ccdc727dae510c5ce64471df937fd
shanker joshi <shankerjosi743@gmail.com>
2024-02-08 20:38:08 +0530

    Add files in chapter 1/

---
+++ chapter 1/variable.py
@@ -0,0 +1,11 @@
+print("This print() function is used to output in terminal and show in screen")
+
+
+# varible
+a = 5
+b = 6
+name = "Shanker"
+print(a)
+print(b)
+print(name)
+print("Here a,b and name are the varible in python")
