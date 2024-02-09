36a93893341495154cc6e6ceb254eb5545223ac3
shanker joshi <shankerjosi743@gmail.com>
2024-02-09 05:53:18 +0530

    Chapter 1 is completed

---
+++ chapter 1/part2.py
@@ -0,0 +1,47 @@
+#  Type casting 
+# a = 10
+# b = 5.3
+# print(a+b)  
+# here the sum is 15.3 they autometic type caste int to float ->(a)
+
+# type conversion
+# x = 32
+# str = "23"
+# str = int(str)
+# print(str+x)
+
+
+#  input in python
+# name = input("Enter your name :")
+# print("Your name is :",name)
+
+# Write a Program to input 2 numbers & print their sum.
+# num1 = int(input("Enter first num: "))
+# print("The value of num1 is : ",num1)
+# num2 = int(input("Enter first num: "))
+# print("The value of num2 is : ",num2)
+# sum = num1+num2
+# print("\nThe value of sum is : ",sum)
+
+
+# WAP to input side of a square & print its area.
+# sideOfSquare = int(input("Enter the side of square : "))
+# print("Area of square of  is : ",sideOfSquare*sideOfSquare)
+
+# WAP to input 2 floating point numbers & print their average.
+# num1 = float(input("Enter first num: "))
+# print("The value of num1 is : ",num1)
+# num2 = float(input("Enter first num: "))
+# print("The value of num2 is : ",num2)
+# avg = (num1+num2)/2
+# print("\nThe value of avg is : ",avg)
+
+# WAP to input 2 int numbers, a and b.
+# Print True if a is greater than or equal to b. If not print False.
+# num1 = int(input("Enter first num: "))
+# print("The value of num1 is : ",num1)
+# num2 = int(input("Enter first num: "))
+# print("The value of num2 is : ",num2)
+# print(num1>num2)
+
+print("End of chapter 1")
--- chapter 1/variable.py
+++ chapter 1/variable.py
@@ -10,14 +10,89 @@ print(b)
 print(name)
 print("Here a,b and name are the varible in python")
 
-name = "sonu"
-age = 23
-price = 99.9
-print(name)
-print(age)
-print(price)
+# name = "sonu"
+# age = 23
+# price = 99.9
+# print(name)
+# print(age)
+# print(price)
 
 # finding type of varible
-print(type(name))
-print(type(age))
-print(type(price))
+# print(type(a))
+# print(type(b))
+# print(type(name))
+is_Adult = True
+value = None
+print(type(is_Adult))
+print(type(value))
+
+#  sum of two numbers..
+num1 = 45
+num2 = 36
+sum = num1 + num2
+print("Sum of two number is :",sum)
+
+# Operators
+"""
+Airthmetic
+Relations/Comparision
+Assignment
+Logical
+"""
+
+# Ex of Airthmetic operator
+# print(80+28)
+# print(80-28)
+# print(80*28)
+# print(80/28)
+# print(80%28)
+# print(8**2)
+
+# Ex of Realational operator
+# print("The value of 5==5 is ",5==5)
+# print("The value of 5!=5 is ",5!=5)
+# print("The value of 5>5 is ",5>5)
+# print("The value of 5>=5 is ",5>=5)
+# print("The value of 5<5 is ",5<5)
+# print("The value of 5<=5 is ",5<=5)
+
+
+# Ex of Assignment operator
+# sumValue = 12
+# sumValue += 12
+# print(sumValue)
+
+# minusValue = 12
+# minusValue -= 12
+# print(minusValue)
+
+# multiplyValue = 12
+# multiplyValue *= 12
+# print(multiplyValue)
+
+# devideValue = 12
+# devideValue /= 12
+# print(devideValue)
+
+# moduloValue = 12
+# moduloValue %= 12
+# print(moduloValue)
+
+# powerValue = 8
+# powerValue **= 3
+# print(powerValue)
+
+
+# Logical Operator
+# print("The value of True And True is :",True and True)
+# print("The value of True And False is :",True and False)
+# print("The value of False And False is :",False and False)
+# print("The value of False And True is :",False and True)
+
+# print("The value of True or True is :",True or True)
+# print("The value of True or False is :",True or False)
+# print("The value of False or False is :",False or False)
+# print("The value of False or True is :",False or True)
+
+# print("The value of not of  True is :",not(True))
+# print("The value of not of  False is :",not(False))
