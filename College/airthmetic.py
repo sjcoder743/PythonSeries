9eb1b9b2fd1a32ccd815a1435e1ab6ad5ee664eb
ektacode <codeharry0@gmail.com>
2024-02-20 06:41:39 +0530

    Aithmetic program added

---
+++ College/airthmetic.py
@@ -0,0 +1,16 @@
+num1 = int(input("Enter number 1: "))
+num2 = int(input("Enter number 2: "))
+
+sum = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+devide = num1 / num2
+power = num1**num2
+modulo = num1 % num2
+
+print("The sum is : ", sum)
+print("The sub is : ", sub)
+print("The mul is : ", mul)
+print("The devide is : ", devide)
+print("The power is : ", power)
+print("The modulo is : ", modulo)
