6e53a6631468318c0e6aeb65447c498073134f14
ektacode <codeharry0@gmail.com>
2024-02-20 19:57:16 +0530

    range program

---
+++ College/range.py
@@ -0,0 +1,20 @@
+# part a
+for i in range(2, 10):
+    print(i)
+print("1 Program ended")
+
+
+# part b
+for i in range(1, 10, 3):
+    print(i)
+print("2 Program ended")
+
+# part c
+for i in range(5):
+    print(i)
+print("3 Program ended")
+
+# part d
+for i in range(1, 15, 2):
+    print(i)
+print("4 Program ended")
