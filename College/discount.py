1f93c5ab5366d962cd4565442618b5274b97f987
Shanker joshi <shankerjoshi743@gmail.com>
2024-03-06 16:46:55 +0530

    update the code of discount.py

--- College/discount.py
+++ College/discount.py
@@ -1,29 +1,10 @@
-def calculate_discount(sales):
-    if sales >= 10000:
-        discount_percentage = 0.10
-    elif sales < 1000:
-        discount_percentage = 0.05
-    else:
-        discount_percentage = 0
-    discount = sales * discount_percentage
-    discounted_price = sales - discount
+sales = int(input("Enter sales: "))
 
-    return discount, discounted_price
+if sales>=10000:
+  dis = int(sales - (sales * 10/100))
+elif sales<10000 and sales >=5000:
+  dis = int(sales - (sales * 5/100))
+else: 
+  print("Opps... There is no discount for less then 5000")
 
-# Get total sales from the user
-try:
-    total_sales = float(input("Enter the total sales amount: $"))
-    
-    # Check if the input is valid (a positive number)
-    if total_sales >= 0:
-        discount, discounted_price = calculate_discount(total_sales)
-        
-        # Display the results
-        print(f"\nOriginal Sales: ${total_sales}")
-        print(f"Discount Percentage: {discount * 100}%")
-        print(f"Discount: ${discount}")
-        print(f"Discounted Price: ${discounted_price}")
-    else:
-        print("Please enter a valid positive number for total sales.")
-except ValueError:
-    print("Invalid input. Please enter a valid number.")
+print("Final price After discount is : ", dis)  
